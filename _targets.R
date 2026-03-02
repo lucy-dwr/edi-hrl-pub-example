da44695461a40fabad727fe88bbab5710b20ac06
Lucy Andrews <lucy.andrews@water.ca.gov>
2026-03-02 13:03:45 -0800

    Fix targets pipeline argument

--- _targets.R
+++ _targets.R
@@ -14,7 +14,7 @@ source("publish/publish.R")
 
 list(
   tar_target(raw_outputs, run_ingest(), format = "file", cue = tar_cue(mode = "always")),
-  tar_target(clean_outputs, run_clean(), format = "file"),
+  tar_target(clean_outputs, run_clean(raw_outputs), format = "file"),
   tar_target(qc_outputs, run_qc(clean_outputs)),
   tar_target(publish_outputs, run_publish(clean_outputs, qc_outputs))
 )
